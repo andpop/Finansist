dc5c4d7e32eaf5df35a816a260677eff20258c8f
Andrey Popov <andpop@mail.ru>
2018-01-08 23:53:34 +0300

    General functions

--- script/app_config.php
+++ script/app_config.php
@@ -1,10 +1,32 @@
 <?php
 require_once 'connection_params.php';
 
-//echo MYSQL_DB;
+mb_internal_encoding("UTF-8");
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+function xss($data)
+{
+    if (is_array($data)) 
+    {
+        $escaped = array();
+        foreach ($data as $key => $value)
+        {
+            $escaped[$key] = xss($value);
+        }
+        return $escaped;
+    }
+    return trim(htmlspecialchars($data));
+}
+
+function redirect($link) 
+{
+    header("Location: $link");
+    exit;
+}
+
 
 function db_connect(){
-	//$mysqli = @new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
 	$mysqli = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
 	
 	if ($mysqli->connect_errno) {
