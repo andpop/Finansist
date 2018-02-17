03100927274bee90f4a574835b037f99813e310e
andpop <andpop@mail.ru>
2018-02-17 21:11:58 +0300

    Delete connection_params_server.php

--- script/connection_params_server.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('MYSQL_SERVER', 'localhost');     
-define('MYSQL_USER', '046434649_finans');
-define('MYSQL_PASSWORD', 'abyfycbcn');    
-define('MYSQL_DB', 'andpop_finansist');   
-?>
