98f16c221c2906b89ec05f188f95f28648c6a474
Andrey Popov <andpop@mail.ru>
2018-02-17 21:25:59 +0300

    Balance - 10

--- 1.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("./script/app_config.php");
-$mysqli = db_connect();
-$mysqli->set_charset("utf8");
-
-$s ='Группа 1 "С кавычками"';
-// echo $s;
-$s1 = $mysqli->real_escape_string($s);
-echo $s1;
